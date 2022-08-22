fc0837f46528b92ec01ed274c63e91023e193a4c
Jobe95 <jonatanbengtsson95@hotmail.se>
2022-08-22 10:15:49 +0200

    Installed dev dependencies and converted project to ES6 syntax

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
-var logger = require('morgan');
-var express = require('express');
-var cookieParser = require('cookie-parser');
-var indexRouter = require('./routes/index');
+import logger from 'morgan';
+import express, { json, urlencoded } from 'express';
+import cookieParser from 'cookie-parser';
+import indexRouter from './routes/index';
 var app = express();
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(json());
+app.use(urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use('/v1', indexRouter);
 
-module.exports = app;
+export default app;
